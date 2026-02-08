d74b21a7bf3923b363edc26688a9695e31eccb79
vojtech <vojta.toth1@seznam.cz>
2026-02-08 23:41:38 +0100

    Quartz sync: Feb 8, 2026, 11:41 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -23,7 +23,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
-    Component.TagList(),
+    // Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
